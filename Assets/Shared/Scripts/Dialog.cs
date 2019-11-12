26d3afd11fdefd3d91e10ddc4b6c5e5d78293781
Alex <alexander.diazrzr@udlap.mx>
2019-11-12 12:37:17 -0600

    Solved issue in Agradecimiento Scene, where the Player could not advance in the Dialog. Cleaned up code.
    Still not sure what the issue was, considering that it was a clear copy of the Scene + Script from the individual project (Unity 2019.X), where the game ran flawlessly.
    Reset Dialog and Audio Manager parameters -> Erased Dialog and Audio Manager -> Replaced Script with Script from Unity 2019 project, but whilst running, executed the original Script with no errors anymore...

--- Assets/Shared/Scripts/Dialog.cs
+++ Assets/Shared/Scripts/Dialog.cs
@@ -12,32 +12,19 @@ public class Dialog : MonoBehaviour
     public int index;
     private int clickCounter=0, sceneIndex;
     public float typingSpeed = 0.02f;
-    //public VideoPlayer videoPlayer;
     public GameObject continueButton;
     public AudioClip[] audios;
     public AudioSource source;
     public GameObject dialogBackground;
     public GameObject endgame;
 
-    //public bool isItWorking;      Serves to determine if an imported Script is incompatible
-
-    // Start is called before the first frame update
     void Start()
-    {
-        Debug.Log("clickCounter original: " + clickCounter);   
-        // Porque luego inicia en un número mayor, por razones desconocidas.
-        clickCounter = 0;
-        Debug.Log("clickCounter asignado: " + clickCounter);   
-
-    
+    {    
         sceneIndex = SceneManager.GetActiveScene().buildIndex;
 
         dialogBackground.SetActive(true);
 
-        Debug.Log("Index: " + index);
-        StartCoroutine(Type());
-        Debug.Log("Number of sentences: " + sentences.Length);
-        Debug.Log("Audios Start: " + audios.Length);
+        StartCoroutine(Type());        
 
         source = GetComponent<AudioSource>();    
     }
@@ -50,26 +37,14 @@ public class Dialog : MonoBehaviour
         }
 
         if (clickCounter==sentences.Length)
-        {
-            //videoPlayer.Play();
-            // Quizás llame al Script LoadScene en otra iteración.
-//            if (sceneIndex == 0)
-
-            //No funciona si se pone SceneIndex++
-            SceneManager.LoadScene(sceneIndex+1);
-
-            if (sceneIndex == 3)
-                endgame.SetActive(true);            
-            /*else if (sceneIndex == 3)
-                endgame.SetActive(true);*/
-
+        {            
+            if (sceneIndex == 9)
+                endgame.SetActive(true);             
         }        
     }
 
     IEnumerator Type()
-    {
-        Debug.Log("Couroutine Audio: " + audios.Length);            
-        //Debug.Log("This index: " + index);
+    { 
         foreach(char letter in sentences[index].ToCharArray())
         {            
             UIText.text += letter;
@@ -79,12 +54,8 @@ public class Dialog : MonoBehaviour
 
     public void NextSentence()
     {
-        //audios = this.audios;
-
-        Debug.Log("clickCounter Pre: " + clickCounter);
         clickCounter++;
-        Debug.Log("clickCounter Pos: " + clickCounter);
-        Debug.Log("Next Sentence Audios: " + audios.Length);                        
+       
         source.PlayOneShot(audios[clickCounter]);
         continueButton.SetActive(false);        
         
@@ -99,13 +70,6 @@ public class Dialog : MonoBehaviour
             UIText.text = "";
             continueButton.SetActive(false);
         }        
-    }
-
-    /* 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }*/
+    } 
 
 }
