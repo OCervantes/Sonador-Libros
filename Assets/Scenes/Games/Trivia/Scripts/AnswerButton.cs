c30fe0b589c3826eb914f7f1a2cf7690c9fd7a65
Mauricio Graciano <mau.graci@gmail.com>
2020-04-23 11:35:19 -0500

    Updated UI in trivia to be more responsive, and updated answer button to be smaller & only include one image

--- Assets/Scenes/Games/Trivia/Scripts/AnswerButton.cs
+++ Assets/Scenes/Games/Trivia/Scripts/AnswerButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AnswerButton : MonoBehaviour {
-	public GameObject imageLeft, imageRight;
+	public GameObject image;
 	public TriviaController triviaController;
 	public Text answerText;
 
@@ -17,31 +17,22 @@ public class AnswerButton : MonoBehaviour {
 	public void Setup(AnswerData data){
 		answerData = data;
 		answerText.text = answerData.answerText;
-		Image imageSpriteLeft =
-			imageLeft.transform.GetChild(0).gameObject.GetComponent<Image>();
-		Image imageSpriteRight =
-			imageRight.transform.GetChild(0).gameObject.GetComponent<Image>();
+		Image imageSprite =
+			image.transform.GetChild(0).gameObject.GetComponent<Image>();
 		if (data.image != null) {
-			imageSpriteLeft.sprite = data.image;
-			imageSpriteRight.sprite = data.image;
-			imageLeft.SetActive(true);
-			imageRight.SetActive(true);
+			imageSprite.sprite = data.image;
+			image.SetActive(true);
 		} else {
-			imageSpriteLeft.sprite = null;
-			imageSpriteRight.sprite = null;
-			imageLeft.SetActive(false);
-			imageRight.SetActive(false);
+			imageSprite.sprite = null;
+			image.SetActive(false);
 		}
 
 		if (data.background != null) {
-			imageLeft.GetComponent<Image>().color = data.background;
-			imageRight.GetComponent<Image>().color = data.background;
+			image.GetComponent<Image>().color = data.background;
 		} else if (data.image == null) {
-			imageLeft.GetComponent<Image>().color = Color.clear;
-			imageRight.GetComponent<Image>().color = Color.clear;
+			image.GetComponent<Image>().color = Color.clear;
 		} else {
-			imageLeft.GetComponent<Image>().color = Color.white;
-			imageRight.GetComponent<Image>().color = Color.white;
+			image.GetComponent<Image>().color = Color.white;
 		}
 	}
 
