f946eb0cfcf557c342061d29ff4dbe52ecebaae5
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2020-05-29 02:10:23 -0500

    Ayuda de nivel, nuevos audios y nuevos anumales con animación
    -Se creo una manita y un letrero que indica que tiene que hacer el usuario.
    -Se activan con un botón, creando el script "desaparecer".
    -También en esta versión se agregaron nnuevos animales con animaciones.

--- Assets/Scenes/Game Intro/Audio_Intro/NoDestruir.cs
+++ Assets/Scenes/Game Intro/Audio_Intro/NoDestruir.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class NoDestruir : MonoBehaviour
 {
+    void Update() {
+        if(SceneManager.GetActiveScene().name == "GameSelector"){
+            Destroy(this.gameObject);
+        }
+    }
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
---
+++ Assets/Scenes/Games/Mat_1/Scripts/desaparecer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desaparecer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sec = 14f;
+    public GameObject hand;
+    public GameObject letrero;
+
+    public int contadorclicks = 0;
+
+    void Start(){
+           desaparecerAnimacion(); 
+    }
+
+    private void RemoverAyuda()
+    {
+        hand.SetActive(false);
+        letrero.SetActive(false);
+        contadorclicks = 0;
+    }
+
+    //Activa el gameobject y luego lo hace desaparecer iniciando corutina 
+    public void desaparecerAnimacion()
+    {
+         if (contadorclicks == 0){
+            contadorclicks = 1;
+            StartCoroutine(LateCall());
+        }
+        else{
+            RemoverAyuda(); //Si vuelve a hacer click durante la animación, se detiene para remover los assets de ayuda.
+            StopAllCoroutines(); //Aunque se remueven los assets la corutina sigue, por lo tanto también se para la corutina.
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator LateCall()
+    {
+        Debug.Log("Si comencé la animación");
+        letrero.SetActive(true);
+        hand.SetActive(true);
+        
+        yield return new WaitForSeconds(sec);
+
+        hand.SetActive(false);
+        letrero.SetActive(false);
+
+        contadorclicks = 0;
+        
+    }
+}
