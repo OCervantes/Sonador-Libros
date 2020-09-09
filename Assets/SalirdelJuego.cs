4e47c510e368e7ac88e1cab001208c6f2e58e44c
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2020-09-09 10:56:53 -0500

    Correcciones en base a primera versión de diagrama de Flujo
    -Correcciones de texto en Game_Intro y el juego de conteo.
    -Se arreglo el slider menu para que estuviera a la derecha y fuera una mariposa. Además se agregaron botones con  imagenes en vez de texto.
    -Agregue en el script Dialog una función para volever a la primera escena. El botón ya se incluyo en donde hay varios dialogos.

---
+++ Assets/SalirdelJuego.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalirdelJuego : MonoBehaviour
+{
+   //Sale de la aplicació´n
+   public void Salir(){
+       Application.Quit();
+   }
+}
