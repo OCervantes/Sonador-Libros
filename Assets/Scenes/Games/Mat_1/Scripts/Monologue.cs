12312efe97b7bbc9197a9c70d4dffa27d2de510f
CrunchyBytes <alexander.diazrz@udlap.mx>
2021-04-30 08:01:41 -0500

    Alter Monologue.cs documentation a bit.

--- Assets/Scenes/Games/Mat_1/Scripts/Monologue.cs
+++ Assets/Scenes/Games/Mat_1/Scripts/Monologue.cs
@@ -8,7 +8,6 @@ public class Monologue : MonoBehaviour
     public Text UIText;
     public GameObject continueButton, dialogBackground, sliderText, slider;    
     public static int fruits, sliderValue;
-    //public FruitInitialization fruitInitializer;
 
     [SerializeField] string sentence;
     [SerializeField] float typingSpeed;    
@@ -16,6 +15,7 @@ public class Monologue : MonoBehaviour
     AudioSource source;    
 
     // Start is called before the first frame update
+    // Initialize variables
     void Start()
     {
         typingSpeed = 0.02f;        
@@ -24,54 +24,54 @@ public class Monologue : MonoBehaviour
 
         source = GetComponent<AudioSource>();        
         
-        Debug.Log("Monologue Total Fruits: " + fruits + "\nSlider Value: " + sliderValue);
+        // See that values given by Basket.cs (fruits) and SliderNumber.cs (sliderValue) are correct.
+        Debug.Log("Monologue Total Fruits: " + fruits + 
+                  "\nSlider Value: " + sliderValue);
 
         StartCoroutine(Type());
     }
 
     IEnumerator Type()
-    { 
+    {
+        // Play instructions audio
         source.PlayOneShot(audios);
 
+        // Display instructions text
         foreach(char letter in sentence.ToCharArray())
         {            
             UIText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
 
-        //Debug.Log("Finished tying.");      
-        continueButton.SetActive(true);            
-                    
+        // Once instructions have finished displaying, the slider and the Continue Button will activate.
+        continueButton.SetActive(true);                  
         sliderText.SetActive(true);
         slider.SetActive(true);        
     }
 
     public void Verify()
     {              
-        /* If the Slider's value concurs with the amount of fruits collected in the previous Scene ("Juego 1"):
-        
-         * The FruitInitialization Script will generate new fruits to collect, once the Player comes back to the "Juego 1"
-           Scene.
-         * Player will be taken to the "Agradecimiento" Scene, to celebrate their achievement.        
-         */     
+        // If the Slider's value concurs with the amount of fruits collected in the previous Scene ("Juego 1"):             
         if (sliderValue == fruits)
         {
-            // This might be redundant, but just in case.
-            //fruitInitializer.SetFruitInitializationFlag(true);
+            /* (This might be redundant, but just in case.)
+             * The FruitInitialization Script will generate new fruits to collect, once the Player comes back to the "Juego 1" Scene.
+            */            
             FruitInitialization.playerCollectedCorrectAmountOfFruits = true;
+
+            // Player will be taken to the "Agradecimiento" Scene, to celebrate their achievement.
             SceneManager.LoadScene("Agradecimiento");
         }
-        /* If not:
-
-         * The FruitInitialization Script will generate the same fruits that were requested this time, in order to have
-           the player practice with the fruits they did not collect correctly.
-         * Player will be taken to the "New Corrección" Scene, in order to learn from their mistakes, and have another go
-           at the game.           
-         */
+        
+        // Otherwise...
         else
         {
-            //fruitInitializer.SetFruitInitializationFlag(false);
+            /* The FruitInitialization Script will generate the same fruits that were requested this time, in order to have the player practice 
+               with the fruits they did not collect correctly.
+             */
             FruitInitialization.playerCollectedCorrectAmountOfFruits = false;
+
+            // Player will be taken to the "New Corrección" Scene, in order to learn from their mistakes, and have another go at the game.
             SceneManager.LoadScene("New Corrección");
         }
     } 
